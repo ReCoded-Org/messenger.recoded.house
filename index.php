d0b8b54abffd0f02587274ff75ef04d49f1f4c2f
Re:Coded <re-coded@re-coded.com>
2019-03-03 10:40:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?
 
+// table for admins
+admin_id
+admin_username
+admin_hash
+timestamp
+
+// table for users
+user_id VARCHAR
+name VARCHAR
+gender VARCHAR
+attributes JSON
+timestamp
+
+// content
+content_id VARCHAR
+message_body JSON
+timestamp
+  
+// images
+image_id VARCHAR
+image_content BLOB
+timestamp
+  
+// message history
+message_id VARCHAR
+from VARCHAR
+to VARCHAR
+content_id VARCHAR
+json VARCHAR
+timestamp
+  
+  
+// Log in
+  
+// Admin interface
+  // View users
+  // View content
+  // Edit individual content
+  // Send
+  // View message history
+  
+// API
+
 ?>
